91afd2344ad789e93910c2f8a1744740bf674506
makima333 <skechdance@gmail.com>
2024-12-14 10:41:49 +0900

    chore: format

--- src/routes/task/+page.svelte
+++ src/routes/task/+page.svelte
@@ -8,33 +8,35 @@
 
 	function createTaskStore() {
 		const { subscribe, set, update } = writable<any>([]);
-		
+
 		return {
 			subscribe,
 			fetchTasks: async () => {
-				const taskDBClient = await TaskDBClient.load("sqlite:mydatabase.db");
+				const taskDBClient = await TaskDBClient.load('sqlite:mydatabase.db');
 				const tasks = await taskDBClient.read();
 				set(tasks);
 			},
 			addTask: async (taskName: string) => {
-				const taskDBClient = await TaskDBClient.load("sqlite:mydatabase.db");
-				const createResponse = await taskDBClient.create(taskName)
-				update((tasks) => [...tasks, createResponse])
+				const taskDBClient = await TaskDBClient.load('sqlite:mydatabase.db');
+				const createResponse = await taskDBClient.create(taskName);
+				update((tasks) => [...tasks, createResponse]);
 			},
 			removeTask: async (taskId: number) => {
-				const taskDBClient = await TaskDBClient.load("sqlite:mydatabase.db");
-				const deleteResponse = await taskDBClient.delete(taskId)
-				update((tasks) => tasks.filter((task: any) => task.id !== taskId))
+				const taskDBClient = await TaskDBClient.load('sqlite:mydatabase.db');
+				const deleteResponse = await taskDBClient.delete(taskId);
+				update((tasks) => tasks.filter((task: any) => task.id !== taskId));
 			},
 			updateTask: async (taskId: number, taskName: string) => {
-				const taskDBClient = await TaskDBClient.load("sqlite:mydatabase.db");
-				const updateResponse = await taskDBClient.update(taskId, taskName)
-				update((tasks) => tasks.map((task: any) => {
-					if (task.id === taskId) {
-						return updateResponse;
-					}
-					return task;
-				}))
+				const taskDBClient = await TaskDBClient.load('sqlite:mydatabase.db');
+				const updateResponse = await taskDBClient.update(taskId, taskName);
+				update((tasks) =>
+					tasks.map((task: any) => {
+						if (task.id === taskId) {
+							return updateResponse;
+						}
+						return task;
+					})
+				);
 			},
 			clearTasks: async () => {
 				set([]);
@@ -43,8 +45,6 @@
 	}
 
 	const taskStore = createTaskStore();
-
-	
 
 	onMount(async () => {
 		console.log('settings page');
@@ -65,7 +65,7 @@
 	<!-- header -->
 	<div class="p-4">
 		<label class="input input-bordered flex items-center gap-2">
-			<input 
+			<input
 				type="text"
 				class="grow"
 				placeholder="Task name"
