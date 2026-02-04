57c1f5bd3ea3570f1ca9a5ef5439012cfd330d05
makima333 <skechdance@gmail.com>
2026-02-04 23:19:36 +0900

    fix: adjust styling for main and drawer components for consistency

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -238,13 +238,10 @@
 	});
 </script>
 
-<main class="drawer drawer-end bg-slate-50 rounded-2xl min-h-screen">
+<main class="drawer drawer-end bg-slate-50 rounded-sm min-h-screen">
 	<input id="my-drawer-2" type="checkbox" class="drawer-toggle" tabindex="-1" />
 	<div class="drawer-content rounded-2xl">
-		<div
-			data-tauri-drag-region
-			class="titlebar h-5 bg-slate-200 flex justify-between rounded-t-2xl"
-		>
+		<div data-tauri-drag-region class="titlebar h-5 bg-slate-200 flex justify-between rounded-t-sm">
 			<div data-tauri-drag-region class="text-black pl-2 text-sm">
 				<span
 					data-tauri-drag-region
@@ -301,7 +298,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="drawer-side rounded-2xl">
+	<div class="drawer-side">
 		<!-- svelte-ignore a11y_click_events_have_key_events -->
 		<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
 		<div
